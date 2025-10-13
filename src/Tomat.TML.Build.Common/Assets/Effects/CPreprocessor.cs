5d35386643e3cf0b8564bbafccad7773714f5e4a
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2025-10-13 16:28:33 -0500

    feat: implement hjson validation and c preprocessor in common project; prepare for compilation analysis

---
+++ src/Tomat.TML.Build.Common/Assets/Effects/CPreprocessor.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Tomat.TML.Build.Common.Assets.Effects;
+
+/// <summary>
+///     Extremely naive parser to handle some useful preprocessor directives.
+///     <br />
+///     Does not aim for accuracy or correctness, only used to optimize some
+///     compilation cases.
+/// </summary>
+public static class CPreprocessor
+{
+    private static readonly Regex include_regex = new(
+        """
+        ^\s*#\s*include\s*"([^"]+)"
+        """,
+        RegexOptions.Compiled
+    );
+
+    /// <summary>
+    ///     Whether a given compilation unit is out-of-date with its compiled
+    ///     binary.
+    /// </summary>
+    public static bool IsOutOfDate(string compilationUnitPath, string binaryPath)
+    {
+        // Always out-of-date if there is no compiled object.
+        if (!File.Exists(binaryPath))
+        {
+            return true;
+        }
+
+        // Traverse includes to determine the latest change made to any of the
+        // text within a compilation unit.
+        var compilationUnitTime = GetLatestWriteTime(compilationUnitPath);
+        var binaryWriteTime = File.GetLastWriteTimeUtc(binaryPath);
+        return compilationUnitTime > binaryWriteTime;
+    }
+
+    private static DateTime GetLatestWriteTime(string filePath)
+    {
+        return GetLatestWriteTimeRecursive(Path.GetFullPath(filePath), []);
+    }
+
+    // Use the earliest possible value to ensure the latest write time is never
+    // overwritten.  This is for ignore or error cases.
+    private static readonly DateTime time_ignore = DateTime.MinValue;
+
+    private static DateTime GetLatestWriteTimeRecursive(string filePath, HashSet<string> visitedFiles)
+    {
+        if (visitedFiles.Contains(filePath))
+        {
+            return time_ignore;
+        }
+
+        if (!File.Exists(filePath))
+        {
+            return time_ignore;
+        }
+
+        visitedFiles.Add(filePath);
+
+        var latest = File.GetLastWriteTimeUtc(filePath);
+
+        // Very naive attempt at resolving imports:
+        // - does not handle cases for #include's path is on another line,
+        // - probably does not handle path traversal incredibly well,
+        // - does not support angle-bracket syntax (unsure if HLSL supports this
+        //   at all)
+        var lines = File.ReadAllLines(filePath);
+        var baseDir = Path.GetDirectoryName(filePath) ?? string.Empty;
+
+        foreach (var line in lines)
+        {
+            var match = include_regex.Match(line);
+            if (!match.Success)
+            {
+                continue;
+            }
+
+            var includePath = match.Groups[1].Value;
+            var fullIncludePath = Path.Combine(baseDir, includePath);
+
+            var includeWriteTime = GetLatestWriteTimeRecursive(fullIncludePath, visitedFiles);
+            if (includeWriteTime > latest)
+            {
+                latest = includeWriteTime;
+            }
+        }
+
+        return latest;
+    }
+}
---
+++ src/Tomat.TML.Build.Common/Assets/Localization/HjsonValidator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Hjson;
+
+namespace Tomat.TML.Build.Common.Assets.Localization;
+
+/// <summary>
+///     Validates <c>.hjson</c> files.
+/// </summary>
+public static class HjsonValidator
+{
+    private static readonly Regex error_regex = new(@"(.*?) At line (.*?), column (.*?) \((.*?)\)", RegexOptions.Compiled);
+
+    /// <summary>
+    ///     Gets the nearest HJSON parsing error in a file.
+    /// </summary>
+    /// <param name="filePath">The file to parse for errors.</param>
+    /// <returns>
+    ///     The nearest error, formatted richly for logging; or,
+    ///     <see langword="null"/>, if there are no errors.
+    /// </returns>
+    public static string? GetNearestErrorInHjsonFile(string filePath)
+    {
+        try
+        {
+            _ = HjsonValue.Load(filePath);
+            return null;
+        }
+        catch (Exception e)
+        {
+            return GetRichErrorMessage(filePath, e.Message);
+        }
+    }
+
+    private static string GetRichErrorMessage(string filePath, string message)
+    {
+        var match = error_regex.Match(message);
+        if (!match.Success)
+        {
+            return $"{filePath}: error HJSON: {message}";
+        }
+
+        var line = match.Groups[2].Value;
+        var column = match.Groups[3].Value;
+        return $"{filePath}({line},{column}): error HJSON: {match.Groups[1].Value}";
+    }
+}
