b981d3eee5ce9290b531312679a30dd76c01b730
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2025-10-17 23:32:17 -0500

    feat: begin setting up infrastructure for launchwrapper

---
+++ src/Tomat.TML.ClientBootstrap/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Tomat.TML.ClientBootstrap;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($"TEST: {string.Join(", ", args)}");
+    }
+}
